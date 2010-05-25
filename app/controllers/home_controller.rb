f2e644b255ff4c3a2bab787036a208d6a38f9197
Julio <juliocgc83@gmail.com>
2010-05-25 19:25:42 -0300

    Ola com tempo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
+    #render :text => "Hello"
+    redirect_to :action => :ola
   end
-
+  
+  def ola
+    
+    @tempo = Time.now
+    
+    
+  end 
+  
 end
