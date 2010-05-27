62672e035e4712c3ef9807360705b403052da874
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-26 22:59:58 -0300

    Button to find

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -220,6 +220,7 @@ end
 
 When /^I search for "(.+)"$/ do |search|
   fill_in "search", :with => search
+  click_button "Find"
 end
 
 Then /^I should see only images with the tag "(.+)"$/ do |searched|
