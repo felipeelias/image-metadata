d3f4ae11e7155b732dc1a69f10cec57b44b59fdf
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-27 23:31:44 -0300

    adittional steps

--- features/step_definitions/images_steps.rb
+++ features/step_definitions/images_steps.rb
@@ -6,3 +6,16 @@ end
 Then /^I should see only images with the tag "(.+)"$/ do |searched|
   pending # express the regexp above with the code you wish you had
 end
+
+Given /^the following images$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see images with the tag "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should not see images with the tag "([^\"]*)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
