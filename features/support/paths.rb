adbeaadec6fc8dd9515287ec33cbc511a775ed37
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-31 22:48:07 -0300

    failing step for search page

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -10,6 +10,8 @@ module NavigationHelpers
 
     when /the home\s?page/
       '/home'
+    when /search page/
+      '/search'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
