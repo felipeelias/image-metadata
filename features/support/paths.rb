b3121c2b37cb166458767296accc25052e1ff88f
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-31 22:55:02 -0300

    little mistake at search path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,7 +11,7 @@ module NavigationHelpers
     when /the home\s?page/
       '/home'
     when /search page/
-      '/search'
+      '/home/search'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
