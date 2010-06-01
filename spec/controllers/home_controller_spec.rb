1389c2109fb4a01e18fcc7611aa3a5ab93b7e00f
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-31 22:56:21 -0300

    search spec for home#search

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe HomeController do
-
-  #Delete these examples and add some real ones
-  it "should use HomeController" do
-    controller.should be_an_instance_of(HomeController)
-  end
-
-
   describe "GET 'search'" do
     it "should be successful" do
       get 'search'
