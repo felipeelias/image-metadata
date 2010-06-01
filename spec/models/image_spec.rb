b8a6e62d7b08499c4a85f6b9bb93c4b8e2c5e3db
Felipe Elias Philipp <felipeelias@gmail.com>
2010-05-31 22:42:57 -0300

    valid attributes test

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -7,4 +7,11 @@ describe Image do
       invalid_image.should_not be_valid
     end    
   end
+
+  context "with valid attributes" do
+    it do
+      invalid_image = Factory.build(:image)
+      invalid_image.should be_valid
+    end    
+  end
 end
