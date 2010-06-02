ca565c993ba92dea686f34c24b8ee401ca96bc4b
Felipe Elias Philipp <felipeelias@gmail.com>
2010-06-01 23:00:48 -0300

    test search a single tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     @images = Image.all
   end
+  
+  def search
+    @images = Image.all(:conditions => { :tags => "cat" })
+  end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,5 +7,16 @@ describe HomeController do
       response.should be_success
       response.should render_template("search")
     end
+    
+    it "should search for 'cat'" do
+      cat = Factory.stub(:image, :tags => "cat")
+      dog = Factory.stub(:image, :tags => "dog")
+
+      Image.should_receive(:all).with(:conditions => {:tags => "cat"}).and_return([cat])
+
+      get 'search', :search => "cat"
+      response.should be_success
+      assigns[:images].should == [cat]
+    end
   end
 end
