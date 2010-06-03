e6683ebeac2b419c1e2d4171069b1bea08db4cdb
Felipe Elias Philipp <felipeelias@gmail.com>
2010-06-03 18:06:09 -0300

    todo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
     @images = Image.all
   end
   
+  # TODO: testar se o parametro é "" ou " " (vazio)
   def search
     @images = Image.tagged_with(params[:search]) if params[:search]
   end
