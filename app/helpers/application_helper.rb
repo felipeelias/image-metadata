a32264100a245d66da06c5efa92e7ec51e93eaef
Felipe Elias Philipp <felipeelias@gmail.com>
2010-09-28 01:38:45 +0100

    ptBR localization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
   def home_page_link
-    link_to "Back to search page", home_path
+    link_to t(:search_page), home_path
+  end
+  
+  def will_paginate(collection, options = {})
+    options.merge!({
+      :previous_label => t(:previous),
+      :next_label => t(:next)
+    })
+
+    super(collection, options)
   end
 end
--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,5 +1,5 @@
 module ImagesHelper
   def link_to_filter(filter_name)
-    link_to filter_name.titleize, :params => { :filter => filter_name }
+    link_to t(filter_name).titleize, :params => { :filter => filter_name }
   end
 end
