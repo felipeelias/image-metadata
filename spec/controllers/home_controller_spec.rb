da1243b188d91c556dbf51891b06bd0d66b06a1e
Felipe Elias Philipp <felipeelias@gmail.com>
2010-09-28 01:55:47 +0100

    tests with i18n

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -25,7 +25,7 @@ describe HomeController do
     
     it "returns a message if the search is empty" do
       get 'search'      
-      response.should have_text(/Try searching for something above/)
+      response.should have_text(%r{#{I18n.translate(:try_search_again)}})
       assigns[:images].should be_nil
     end
 
@@ -33,7 +33,7 @@ describe HomeController do
       Image.should_receive(:tagged_with).with("invalid").and_return([])
       
       get 'search', :search => "invalid"
-      response.should have_text(/no itens found/)
+      response.should have_text(%r{#{I18n.translate(:no_itens_found)}})
       assigns[:images].should be_empty
     end
   end
@@ -44,7 +44,7 @@ describe HomeController do
     it "should display a count of untagged images" do
       Image.should_receive(:count).and_return(2)
       get :index
-      response.should have_tag("span.untagged", :text => "2 images")
+      response.should have_tag("span.untagged", :text => I18n.translate(:image, :count => 2))
     end
   end
 end
